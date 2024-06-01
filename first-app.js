d8911612e06dec465ccfb97d246f920efb60cba2
iamsamiadnan <sami.git@aol.com>
2024-06-01 23:06:32 +0600

    get request

--- first-app.js
+++ first-app.js
@@ -7,16 +7,37 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-   
+    const headers = req.headers;
+    console.log(url)
 
-
-    if (url === '/about') {
+    if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<h1>Hello!!</h1>');
-        res.end();
+        res.write('<html>');
+        res.write('<head><title>this is a title</title></head>');
+        res.write('<body>');
+        res.write('<form action="/submission" method="GET">');
+        res.write('<input type="text" name="name">');
+        res.write('<input type="text" name="age">');
+        res.write('<button type="submit">Submit</button>');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
 
     }
-    res.write('<h1>this is a default page</h1>')
+    //submission?name=adnan+sami&age=25
+    //[0] = submission?
+    //[1] = adnan+sami&age=25
+    console.log(url.split("name="))
+    if(url.split("name=")[0] === '/submission?') {
+        
+        res.write(`<h1>welcome ${url.split("name=")[1].replace("+", " ")}</h2>`);
+        return res.end();
+    }
+
+    
+
+    res.write('<h1>404 not found page</h1>')
     res.end();
 
     
